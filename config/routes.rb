ce91dc400c59c7245bb72f5b207a073c01199a12
yuma <ikeoku.y@gmail.com>
2021-06-16 19:12:19 +0900

    ユーザー詳細ページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show]
+  
   devise_for :users
   resources :books
   root to: 'books#index'
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
