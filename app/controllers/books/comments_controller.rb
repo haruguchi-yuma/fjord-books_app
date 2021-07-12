7ccd294f04604df35a8fc406205738747699e116
yuma <ikeoku.y@gmail.com>
2021-07-12 21:44:10 +0900

    rubocopによる指摘を修正

--- app/controllers/books/comments_controller.rb
+++ app/controllers/books/comments_controller.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Books::CommentsController < CommentsController
-  before_action :set_commentable, only: %i[create]
-  
+  before_action :set_commentable
+
   private
 
   def set_commentable
