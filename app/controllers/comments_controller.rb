c04bac1eec44fee7ae65e36831f4aff13f6dd928
yuma <ikeoku.y@gmail.com>
2021-07-12 17:11:22 +0900

    入力フォームからコメントを入力できるようにした

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+  before_action :comment_params
+  def create
+    @comment = @commentable.comments.build(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    else
+      render @commentable
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
