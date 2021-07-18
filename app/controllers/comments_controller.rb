b78b5011dd9bfc9222a54f08ec0ac56502796438
yuma <ikeoku.y@gmail.com>
2021-07-18 11:19:22 +0900

    コメントを保存するときのエラーに対処

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,12 +3,12 @@
 class CommentsController < ApplicationController
   before_action :comment_params
   def create
-    @comment = @commentable.comments.build(comment_params)
+    @comment = p @commentable.comments.build(comment_params)
     @comment.user_id = current_user.id
     if @comment.save
       redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
     else
-      render @commentable
+      redirect_to @commentable
     end
   end
 
