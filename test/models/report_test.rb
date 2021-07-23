2596f188370f199d087ca462c34bd470fc5ce24d
yuma <ikeoku.y@gmail.com>
2021-07-23 11:27:39 +0900

    インスタンス変数からローカル変数に変更

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -4,19 +4,19 @@ require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
   test '#editable?' do
-    @alice = users(:alice)
-    @bob = users(:bob)
-    @report = reports(:by_alice)
+    alice = users(:alice)
+    bob = users(:bob)
+    report = reports(:by_alice)
 
-    assert_not @report.editable?(@bob)
-    assert @report.editable?(@alice)
+    assert_not report.editable?(bob)
+    assert report.editable?(alice)
   end
 
   test '#created_on' do
-    @report = reports(:by_alice)
-    @report.created_at = 'Sat, 17 Jul 2021 14:16:52.298299000 JST +09:00'
+    report = reports(:by_alice)
+    report.created_at = 'Sat, 17 Jul 2021 14:16:52.298299000 JST +09:00'
 
-    assert_not_equal 'Sat, 17 Jul 2021', @report.created_on
-    assert_equal 'Sat, 17 Jul 2021'.to_date, @report.created_on
+    assert_not_equal 'Sat, 17 Jul 2021', report.created_on
+    assert_equal 'Sat, 17 Jul 2021'.to_date, report.created_on
   end
 end
